27b8f507dbdc982955a2f7f57d1bee4d2bd82cb1
Harrison <harrisonhough4@gmail.com>
2022-11-03 15:03:26 +0200

    chore: moved analytics and UI

---
+++ Editor/EditorMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace ReadyPlayerMe.AvatarLoader.Editor
+{
+    public static class EditorMenu
+    {
+        [MenuItem("Ready Player Me/Avatar Loader", priority = 0)]
+        public static void OpenAvatarLoaderWindow()
+        {
+            AvatarLoaderEditorWindow.ShowWindowMenu();
+        }
+    }
+}
