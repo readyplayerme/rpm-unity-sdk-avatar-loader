69e521483cff75ea79ef92736fcb94811a039667
Harrison <harrisonhough4@gmail.com>
2022-11-25 11:14:15 +0200

    chore: removed initialize on load

--- Editor/ShaderHelper.cs
+++ Editor/ShaderHelper.cs
@@ -18,12 +18,6 @@ public static class ShaderHelper
     private const string URP_SHADERS = "Packages/com.readyplayerme.avatarloader/Shaders/DefaultglTFastShadersURP.shadervariants";
 #endif
 
-    [InitializeOnLoadMethod]
-    private static void InitializeOnLoad()
-    {
-        AddPreloadShaders();
-    }
-    
     public static void AddPreloadShaders()
     {
         var graphicsSettings = AssetDatabase.LoadAssetAtPath<GraphicsSettings>(GRAPHICS_SETTING_PATH);
