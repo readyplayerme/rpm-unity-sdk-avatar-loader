db5322d721d610b58b006e650feb9f485cdcf01e
Harrison <harrisonhough4@gmail.com>
2022-11-28 11:11:09 +0200

    test: fix for null ref

--- Editor/ShaderHelper.cs
+++ Editor/ShaderHelper.cs
@@ -1,4 +1,4 @@
-﻿using UnityEditor;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -58,7 +58,8 @@ public static class ShaderHelper
             shaderInArray.objectReferenceValue = shaderVariants;
             serializedGraphicsObject.ApplyModifiedProperties();
         }
-
-        AssetDatabase.SaveAssets();
+        
+        Object.DestroyImmediate(shaderVariants);
+        Object.DestroyImmediate(serialize.context);
     }
 }
