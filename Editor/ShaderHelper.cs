658a67f93912811ada4235f31087be00dcb8a09e
Harrison <harrisonhough4@gmail.com>
2022-11-28 15:05:04 +0200

    chore: fix include

--- Editor/ShaderHelper.cs
+++ Editor/ShaderHelper.cs
@@ -1,77 +1,82 @@
-﻿using ReadyPlayerMe.Core;
+﻿using ReadyPlayerMe.Core.Editor;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public static class ShaderHelper 
+namespace ReadyPlayerMe.AvatarLoader.Editor
 {
-    private const string INCLUDE_SHADER_PROPERTY = "m_PreloadedShaders";
-    private const string GRAPHICS_SETTING_PATH = "ProjectSettings/GraphicsSettings.asset";
+    public static class ShaderHelper
+    {
+        private const string INCLUDE_SHADER_PROPERTY = "m_PreloadedShaders";
+        private const string GRAPHICS_SETTING_PATH = "ProjectSettings/GraphicsSettings.asset";
 #if DISABLE_AUTO_INSTALLER
-    private const string STANDARD_SHADERS = "Assets/Ready Player Me/Avatar Loader/Shaders/DefaultglTFastShaders.shadervariants";
+        private const string STANDARD_SHADERS = "Assets/Ready Player Me/Avatar Loader/Shaders/DefaultglTFastShaders.shadervariants";
 #else
     private const string STANDARD_SHADERS = "Packages/com.readyplayerme.avatarloader/Shaders/DefaultglTFastShaders.shadervariants";
 #endif
-        
+
 #if DISABLE_AUTO_INSTALLER
-    private const string URP_SHADERS = "Assets/Ready Player Me/Avatar Loader/Shaders/DefaultglTFastShadersURP.shadervariants";
+        private const string URP_SHADERS = "Assets/Ready Player Me/Avatar Loader/Shaders/DefaultglTFastShadersURP.shadervariants";
 #else
     private const string URP_SHADERS = "Packages/com.readyplayerme.avatarloader/Shaders/DefaultglTFastShadersURP.shadervariants";
 #endif
 
-    [InitializeOnLoadMethod]
-    private static void InitializeOnLoad()
-    {
-        Debug.Log("SHADER HELPER INIT");
-        ModuleInstaller.ModuleInstallComplete += AddPreloadShaders;
-    }
-    
-    public static void AddPreloadShaders()
-    {
-        Debug.Log("Run add preload shaders");
-        var graphicsSettings = AssetDatabase.LoadAssetAtPath<GraphicsSettings>(GRAPHICS_SETTING_PATH);
-        var serializedGraphicsObject = new SerializedObject(graphicsSettings);
-
-        SerializedProperty shaderIncludeArray = serializedGraphicsObject.FindProperty(INCLUDE_SHADER_PROPERTY);
-        if (shaderIncludeArray == null)
-            return;
-
-        var renderPipelineAsset = GraphicsSettings.defaultRenderPipeline;
-        string shaderPath = renderPipelineAsset == null ? STANDARD_SHADERS :  URP_SHADERS;
-        AssetDatabase.Refresh();
-        var shaderVariants = AssetDatabase.LoadAssetAtPath<ShaderVariantCollection>(shaderPath);
-
-        if (shaderVariants == null)
+        [InitializeOnLoadMethod]
+        private static void InitializeOnLoad()
         {
-            Debug.Log($"Shader variants null Path: {shaderPath}");
-            serializedGraphicsObject.Dispose();
-            return;
+            Debug.Log("SHADER HELPER INIT");
+#if !DISABLE_AUTO_INSTALLER
+            ModuleInstaller.ModuleInstallComplete += AddPreloadShaders;
+#endif
         }
 
-        var newArrayIndex = shaderIncludeArray.arraySize;
-        var shadersIncluded = false;
-        var serializedVariants = new SerializedObject(shaderVariants);
-        
-        foreach (SerializedProperty shaderInclude in shaderIncludeArray)
+        public static void AddPreloadShaders()
         {
-            if (shaderInclude.objectReferenceValue.name == serializedVariants.targetObject.name)
+            Debug.Log("Run add preload shaders");
+            var graphicsSettings = AssetDatabase.LoadAssetAtPath<GraphicsSettings>(GRAPHICS_SETTING_PATH);
+            var serializedGraphicsObject = new SerializedObject(graphicsSettings);
+
+            SerializedProperty shaderIncludeArray = serializedGraphicsObject.FindProperty(INCLUDE_SHADER_PROPERTY);
+            if (shaderIncludeArray == null)
+                return;
+
+            var renderPipelineAsset = GraphicsSettings.defaultRenderPipeline;
+            string shaderPath = renderPipelineAsset == null ? STANDARD_SHADERS : URP_SHADERS;
+            AssetDatabase.Refresh();
+            var shaderVariants = AssetDatabase.LoadAssetAtPath<ShaderVariantCollection>(shaderPath);
+
+            if (shaderVariants == null)
             {
-                Debug.Log("glTFast shader variants found in Graphics Settings->Preloaded Shaders");
-                shadersIncluded = true;
-                break;
+                Debug.Log($"Shader variants null Path: {shaderPath}");
+                serializedGraphicsObject.Dispose();
+                return;
             }
-        }
-        
-        if (!shadersIncluded)
-        {
-            Debug.Log($"Added {shaderVariants.name} to Preloaded Shaders in Graphics Settings ");
 
-            shaderIncludeArray.InsertArrayElementAtIndex(newArrayIndex);
-            SerializedProperty shaderInArray = shaderIncludeArray.GetArrayElementAtIndex(newArrayIndex);
-            shaderInArray.objectReferenceValue = shaderVariants;
-            serializedGraphicsObject.ApplyModifiedProperties();
+            var newArrayIndex = shaderIncludeArray.arraySize;
+            var shadersIncluded = false;
+            var serializedVariants = new SerializedObject(shaderVariants);
+
+            foreach (SerializedProperty shaderInclude in shaderIncludeArray)
+            {
+                if (shaderInclude.objectReferenceValue.name == serializedVariants.targetObject.name)
+                {
+                    Debug.Log("glTFast shader variants found in Graphics Settings->Preloaded Shaders");
+                    shadersIncluded = true;
+                    break;
+                }
+            }
+
+            if (!shadersIncluded)
+            {
+                Debug.Log($"Added {shaderVariants.name} to Preloaded Shaders in Graphics Settings ");
+
+                shaderIncludeArray.InsertArrayElementAtIndex(newArrayIndex);
+                SerializedProperty shaderInArray = shaderIncludeArray.GetArrayElementAtIndex(newArrayIndex);
+                shaderInArray.objectReferenceValue = shaderVariants;
+                serializedGraphicsObject.ApplyModifiedProperties();
+            }
+            AssetDatabase.SaveAssets();
+            serializedVariants.Dispose();
         }
-        AssetDatabase.SaveAssets();
-        serializedVariants.Dispose();
     }
 }
