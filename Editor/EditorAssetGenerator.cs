a4596653a4d20beae7644a64e8e78174aa250c58
Robin <1121080+rYuuk@users.noreply.github.com>
2022-12-13 15:15:00 +0100

    fix: add generation of settings

---
+++ Editor/EditorAssetGenerator.cs
@@ -0,0 +1,30 @@
+﻿using ReadyPlayerMe.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace ReadyPlayerMe.AvatarLoader.Editor
+{
+    public class EditorAssetGenerator
+    {
+        private const string SETTINGS_SAVE_FOLDER = "Ready Player Me/Resources/Settings";
+        private const string AVATAR_LOADER_ASSET_NAME = "AvatarLoaderSettings.asset";
+
+        public static void CreateSettingsAssets()
+        {
+            DirectoryUtility.ValidateDirectory($"{Application.dataPath}/{SETTINGS_SAVE_FOLDER}");
+            AssetDatabase.Refresh();
+            CreateAvatarLoaderSettings();
+        }
+
+        private static void CreateAvatarLoaderSettings()
+        {
+            var newSettings = ScriptableObject.CreateInstance<AvatarLoaderSettings>();
+            newSettings.AvatarConfig = null;
+            newSettings.AvatarCachingEnabled = DefaultSettings.AvatarCachingEnabled;
+
+            AssetDatabase.CreateAsset(newSettings, $"Assets/{SETTINGS_SAVE_FOLDER}/{AVATAR_LOADER_ASSET_NAME}");
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
+
