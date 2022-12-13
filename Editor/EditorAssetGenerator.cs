10d8111853f1c11c0230f71716f95a7765c3ced5
Robin <1121080+rYuuk@users.noreply.github.com>
2022-12-13 15:26:30 +0100

    chore: subscribe to RegisteredPackage event

--- Editor/EditorAssetGenerator.cs
+++ Editor/EditorAssetGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using ReadyPlayerMe.Core;
+using ReadyPlayerMe.Core.Editor;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,11 +10,17 @@ namespace ReadyPlayerMe.AvatarLoader.Editor
         private const string SETTINGS_SAVE_FOLDER = "Ready Player Me/Resources/Settings";
         private const string AVATAR_LOADER_ASSET_NAME = "AvatarLoaderSettings.asset";
 
+        static EditorAssetGenerator()
+        {
+            ModuleInstaller.RegisteredPackage += CreateSettingsAssets;
+        }
+        
         public static void CreateSettingsAssets()
         {
             DirectoryUtility.ValidateDirectory($"{Application.dataPath}/{SETTINGS_SAVE_FOLDER}");
             AssetDatabase.Refresh();
             CreateAvatarLoaderSettings();
+            ModuleInstaller.RegisteredPackage -= CreateSettingsAssets;
         }
 
         private static void CreateAvatarLoaderSettings()
