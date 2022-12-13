1e7f679c601f4998aeae592d25de2e862449cf44
Robin <1121080+rYuuk@users.noreply.github.com>
2022-12-13 15:36:31 +0100

    feat: add event callbacks for when package is registered

--- Editor/EditorAssetGenerator.cs
+++ Editor/EditorAssetGenerator.cs
@@ -1,26 +1,38 @@
-﻿using ReadyPlayerMe.Core;
-using ReadyPlayerMe.Core.Editor;
+﻿using System.Linq;
+using ReadyPlayerMe.Core;
 using UnityEditor;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 namespace ReadyPlayerMe.AvatarLoader.Editor
 {
+    [InitializeOnLoad]
     public class EditorAssetGenerator
     {
         private const string SETTINGS_SAVE_FOLDER = "Ready Player Me/Resources/Settings";
         private const string AVATAR_LOADER_ASSET_NAME = "AvatarLoaderSettings.asset";
+        private const string AVATAR_LOADER_MODULE_NAME = "com.readyplayerme.avatarloader";
 
         static EditorAssetGenerator()
         {
-            ModuleInstaller.RegisteredPackage += CreateSettingsAssets;
+            Events.registeredPackages += OnRegisteredPackages;
         }
-        
-        public static void CreateSettingsAssets()
+
+        private static void OnRegisteredPackages(PackageRegistrationEventArgs args)
+        {
+            Events.registeredPackages -= OnRegisteredPackages;
+            if (args.added != null && args.added.Any(p => p.name == AVATAR_LOADER_MODULE_NAME))
+            {
+                Events.registeredPackages -= OnRegisteredPackages;
+                CreateSettingsAssets();
+            }
+        }
+
+        private static void CreateSettingsAssets()
         {
             DirectoryUtility.ValidateDirectory($"{Application.dataPath}/{SETTINGS_SAVE_FOLDER}");
             AssetDatabase.Refresh();
             CreateAvatarLoaderSettings();
-            ModuleInstaller.RegisteredPackage -= CreateSettingsAssets;
         }
 
         private static void CreateAvatarLoaderSettings()
