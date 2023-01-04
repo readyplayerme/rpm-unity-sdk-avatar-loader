13c7cc7ecaf587178cac5b35d6af537e055cec65
Harrison <harrisonhough4@gmail.com>
2023-01-04 16:18:31 +0200

    chore: inline docs + minor fixes

--- Runtime/Utils/AvatarAnimatorHelper.cs
+++ Runtime/Utils/AvatarAnimatorHelper.cs
@@ -1,13 +1,23 @@
-﻿using ReadyPlayerMe.Core;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace ReadyPlayerMe.AvatarLoader
 {
+    /// <summary>
+    ///     This static class contains useful helper functions used in the <see cref="ReadyPlayerMe" /> API.
+    /// </summary>
     public static class AvatarAnimatorHelper
     {
         private const string ANIMATOR_CONTROLLER_NAME = "Avatar Animator";
         private static RuntimeAnimatorController animatorController;
 
+        /// <summary>
+        ///     Loads and sets the <see cref="RuntimeAnimatorController" /> property of the <c>GameObjects</c>'s
+        ///     <see cref="Animator" /> component.
+        /// </summary>
+        /// <param name="bodyType">
+        ///     The avatar body type. EG <see cref="BodyType.FullBody" /> or <see cref="BodyType.HalfBody" />
+        /// </param>
+        /// <param name="avatar"></param>
         public static void SetupAnimator(BodyType bodyType, GameObject avatar)
         {
             if (bodyType != BodyType.FullBody)
@@ -21,7 +31,10 @@ namespace ReadyPlayerMe.AvatarLoader
             }
 
             var animator = avatar.GetComponent<Animator>();
-            animator.runtimeAnimatorController = animatorController;
+            if (animator != null)
+            {
+                animator.runtimeAnimatorController = animatorController;
+            }
         }
     }
 }
--- Runtime/Utils/AvatarConfigProcessor.cs
+++ Runtime/Utils/AvatarConfigProcessor.cs
@@ -3,10 +3,22 @@ using ReadyPlayerMe.Core;
 
 namespace ReadyPlayerMe.AvatarLoader
 {
+    /// <summary>
+    ///     This class is responsible for the <see cref="AvatarConfig" />.
+    /// </summary>
     public static class AvatarConfigProcessor
     {
         private const string TAG = nameof(AvatarConfigProcessor);
 
+        private const string PARAM_TRUE = "true";
+        private const string PARAM_FALSE = "false";
+
+        /// <summary>
+        ///     This method converts the <see cref="AvatarConfig" /> data and combines it into a <c>string</c> that can be added to
+        ///     an avatar URL.
+        /// </summary>
+        /// <param name="avatarConfig">Stores the settings of the <see cref="AvatarConfig" /> to use when requesting the avatar.</param>
+        /// <returns>The <see cref="AvatarConfig" /> parameters combined as a <c>string</c>.</returns>
         public static string ProcessAvatarConfiguration(AvatarConfig avatarConfig)
         {
             SDKLogger.Log(TAG, "Processing Avatar Configuration");
@@ -16,15 +28,25 @@ namespace ReadyPlayerMe.AvatarLoader
                    $"&textureAtlas={AvatarConfigMap.TextureAtlas[avatarConfig.TextureAtlas]}" +
                    $"&textureSizeLimit={ProcessTextureSizeLimit(avatarConfig.TextureSizeLimit)}" +
                    $"{ProcessMorphTargets(avatarConfig.MorphTargets)}" +
-                   $"&useHands={(avatarConfig.UseHands ? "true" : "false")}" +
-                   $"&useDracoMeshCompression={(avatarConfig.UseDracoCompression ? "true" : "false")}";
+                   $"&useHands={(avatarConfig.UseHands ? PARAM_TRUE: PARAM_FALSE)}" +
+                   $"&useDracoMeshCompression={(avatarConfig.UseDracoCompression ? PARAM_TRUE : PARAM_FALSE)}";
         }
-        
+
+        /// <summary>
+        ///     Processes the processes the <paramref name="textureSize" /> and ensures it is a valid value.
+        /// </summary>
+        /// <param name="textureSize">The value to process.</param>
+        /// <returns>A validated <c>int</c>/returns>
         private static int ProcessTextureSizeLimit(int textureSize)
         {
             return textureSize % 2 == 0 ? textureSize : textureSize + 1;
         }
-        
+
+        /// <summary>
+        ///     Combines the list of strings in <paramref name="targets" /> into a single valid morph target parameter.
+        /// </summary>
+        /// <param name="targets">A list of morph targets as strings.</param>
+        /// <returns>A query string of combined morph targets.</returns>
         private static string ProcessMorphTargets(List<string> targets)
         {
             if (targets.Count == 0)
