6e924c8e3a583f73d80741e4a41f83f038ee9da0
Robin <1121080+rYuuk@users.noreply.github.com>
2023-04-27 17:07:44 +0200

    feat: remove redundant coroutine extensions

--- Runtime/Utils/ExtensionMethods.cs
+++ Runtime/Utils/ExtensionMethods.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -42,50 +41,6 @@ namespace ReadyPlayerMe.AvatarLoader
             var json = JsonConvert.SerializeObject(metadata);
             File.WriteAllText(path, json);
         }
-
-        #region Coroutine Runner
-
-        [ExecuteInEditMode]
-        public class CoroutineRunner : MonoBehaviour
-        {
-            ~CoroutineRunner()
-            {
-                Destroy(gameObject);
-            }
-        }
-
-        private static CoroutineRunner operation;
-
-        private const HideFlags HIDE_FLAGS = HideFlags.DontSaveInEditor | HideFlags.HideInHierarchy |
-                                             HideFlags.HideInInspector | HideFlags.NotEditable |
-                                             HideFlags.DontSaveInBuild;
-
-        public static Coroutine Run(this IEnumerator iEnumerator)
-        {
-            CoroutineRunner[] operations = Resources.FindObjectsOfTypeAll<CoroutineRunner>();
-            if (operations.Length == 0)
-            {
-                operation = new GameObject("[CoroutineRunner]").AddComponent<CoroutineRunner>();
-                operation.hideFlags = HIDE_FLAGS;
-                operation.gameObject.hideFlags = HIDE_FLAGS;
-            }
-            else
-            {
-                operation = operations[0];
-            }
-
-            return operation.StartCoroutine(iEnumerator);
-        }
-
-        public static void Stop(this Coroutine coroutine)
-        {
-            if (operation != null)
-            {
-                operation.StopCoroutine(coroutine);
-            }
-        }
-
-        #endregion
 
         #region Get Picker
 
