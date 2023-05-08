d161a8fbf8179edf0fda9b4e1c52ab09e15d9ac8
Robin <1121080+rYuuk@users.noreply.github.com>
2023-05-08 12:30:41 +0200

    [SDK-140] Add setup guide window (#85)
    ## [SDK-140](https://ready-player-me.atlassian.net/browse/SDK-140)
    
    ## Dependencies
    ```Package
    Core: feature/setup-wizard
    ```
    
    ## Changes
    
    #### Added
    
    - Setup guide with panels for subdomain, avatar config and analytics 
    
    #### Updated
    
    - Extracted field for selecting avatar config
    
    #### Removed
    
    - Analytics window 
    - Quick panel window

---
+++ Editor/UI/EditorWindows/SetupGuideWindow.cs
@@ -0,0 +1,204 @@
+using ReadyPlayerMe.Core.Analytics;
+using ReadyPlayerMe.Core.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace ReadyPlayerMe.AvatarLoader.Editor
+{
+    [InitializeOnLoad]
+    public class SetupGuideWindow : EditorWindow
+    {
+        private const string WINDOW_NAME = "Setup Guide";
+        private const int BUTTON_FONT_SIZE = 12;
+
+        public const string FIRST_TIME_SETUP_DONE = "first-time-setup";
+        private const string BACK_BUTTON_LABEL = "Back";
+        private const string FINISH_SETUP_BUTTON_LABEL = "Finish Setup";
+        private const string OPEN_QUICKSTART_SCENE_BUTTON_LABEL = "Open QuickStart Scene";
+
+        private Header header;
+        private IEditorWindowComponent[] panels;
+        private int currentPanelIndex;
+
+        /// <summary>
+        ///     Constructor method that subscribes to the StartUp event.
+        /// </summary>
+        static SetupGuideWindow()
+        {
+            EntryPoint.Startup += OnStartup;
+        }
+
+        /// <summary>
+        ///     This method is called when Unity Editor is closed or this package is removed.
+        /// </summary>
+        private void OnDestroy()
+        {
+            EntryPoint.Startup -= OnStartup;
+        }
+
+        /// <summary>
+        ///     This method is called when a Unity project is opened or after this Unity package has finished importing and is
+        ///     responsible for displaying the window. It also calls analytics events if enabled.
+        /// </summary>
+        private static void OnStartup()
+        {
+            if (CanShowWindow())
+            {
+                AnalyticsEditorLogger.Enable();
+                ShowWindow();
+            }
+
+            if (AnalyticsEditorLogger.IsEnabled)
+            {
+                AnalyticsEditorLogger.EventLogger.LogOpenProject();
+                AnalyticsEditorLogger.EventLogger.IdentifyUser();
+                EditorApplication.quitting += OnQuit;
+            }
+        }
+
+        private static bool CanShowWindow()
+        {
+            return !ProjectPrefs.GetBool(FIRST_TIME_SETUP_DONE);
+        }
+
+        /// <summary>
+        ///     This method is called when the Unity Editor is closed and logs the close event.
+        /// </summary>
+        private static void OnQuit()
+        {
+            AnalyticsEditorLogger.EventLogger.LogCloseProject();
+        }
+
+        [MenuItem("Ready Player Me/Re-run Setup")]
+        public static void ShowWindow()
+        {
+            GetWindow(typeof(SetupGuideWindow), false, WINDOW_NAME);
+            ProjectPrefs.SetBool(FIRST_TIME_SETUP_DONE, true);
+        }
+
+        /// <summary>
+        ///     This method the panels and banner if not set already and sets the button event listeners
+        /// </summary>
+        private void LoadPanels()
+        {
+            panels ??= new IEditorWindowComponent[]
+            {
+                new SubdomainPanel(),
+                new AvatarConfigPanel(),
+                new AnalyticsPanel(),
+            };
+
+            header ??= new Header();
+        }
+
+        private void OnGUI()
+        {
+            LoadPanels();
+            DrawContent();
+        }
+
+        private GUIStyle GetButtonStyle()
+        {
+            return new GUIStyle(GUI.skin.button)
+            {
+                fontStyle = FontStyle.Bold,
+                fontSize = BUTTON_FONT_SIZE,
+                padding = new RectOffset(10, 10, 5, 5),
+                fixedHeight = 30,
+                stretchWidth = true,
+            };
+        }
+
+        private void DrawContent()
+        {
+            minSize = new Vector2(460, 400);
+
+            Layout.Horizontal(() =>
+            {
+                GUILayout.FlexibleSpace();
+
+                Layout.Vertical(() =>
+                {
+                    header.Draw(position);
+                    panels[currentPanelIndex].Draw(position);
+                    GUILayout.FlexibleSpace();
+                    DrawFooter(panels[currentPanelIndex]);
+                }, false, GUILayout.Height(400));
+            });
+        }
+
+        private void DrawFooter(IEditorWindowComponent panel)
+        {
+            Layout.Horizontal(() =>
+            {
+                if (!(panel is SubdomainPanel))
+                {
+                    GUILayout.Space(15);
+                    if (GUILayout.Button(BACK_BUTTON_LABEL, GetButtonStyle()))
+                    {
+                        OnBackButton();
+                    }
+                }
+
+                GUILayout.FlexibleSpace();
+
+                if (panel is AnalyticsPanel)
+                {
+                    if (GUILayout.Button(FINISH_SETUP_BUTTON_LABEL, GetButtonStyle()))
+                    {
+                        OnFinishSetup();
+                    }
+
+                    if (GUILayout.Button(OPEN_QUICKSTART_SCENE_BUTTON_LABEL, GetButtonStyle()))
+                    {
+                        OnOpenQuickStartScene();
+                    }
+                }
+                else
+                {
+                    if (panel is SubdomainPanel { IsSubdomainFieldEmpty: true })
+                    {
+                        GUI.enabled = false;
+                    }
+
+                    if (panel is AvatarConfigPanel { IsAvatarConfigFieldEmpty: true })
+                    {
+                        GUI.enabled = false;
+                    }
+
+                    if (GUILayout.Button("Next", GetButtonStyle()))
+                    {
+                        OnNextButton();
+                    }
+                    GUI.enabled = true;
+                }
+
+                GUILayout.Space(15);
+
+            }, true, GUILayout.Width(460));
+        }
+
+        private void OnBackButton()
+        {
+            currentPanelIndex--;
+        }
+
+        private void OnNextButton()
+        {
+            currentPanelIndex++;
+        }
+
+        private void OnFinishSetup()
+        {
+            Close();
+        }
+
+        private void OnOpenQuickStartScene()
+        {
+            if (!new QuickStartHelper().Open())
+            {
+                EditorUtility.DisplayDialog(WINDOW_NAME,"No quick start sample found.","OK" );
+            }
+        }
+    }
+}
